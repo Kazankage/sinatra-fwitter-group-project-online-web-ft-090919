749970ab70cb1d5882f31396df8f4e0cae399bf0
Randel Jiao <randeljiao@gmail.com>
2019-10-27 03:42:23 +0000

    Automatically backed up by Learn

--- db/migrate/20191027034209_create_users_table.rb.rb
+++ db/migrate/20191027034209_create_users_table.rb.rb
@@ -1,4 +1,11 @@
-class CreateUsersTable.rb < ActiveRecord::Migration
+class CreateUsersTable < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :username
+      t.text :email
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
   end
 end
